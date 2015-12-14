92b957ced86649bab6e2b2d5282db827d3524f7b
bsh <b.shirindeli@gmail.com>
2015-12-14 16:08:42 +0330

    Add fragment_devicelisr_drawer to layout

---
+++ app/src/main/java/com/doubleteam/healthcare/bluetoothSevice/bluetoothTransactionService.java
@@ -0,0 +1,11 @@
+package com.doubleteam.healthcare.bluetoothSevice;
+
+/**
+ * Created by bsh on 12/14/2015.
+ */
+public class bluetoothTransactionService {
+    private static final String TAG="bluetoothTransactionService";
+
+
+
+}
